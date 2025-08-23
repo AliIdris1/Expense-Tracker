a8a00cde11fa724dfd5bcb97862818cd8acc04c6
AliIdris1 <aliknee123@gmail.com>
2025-08-23 06:23:45 +0300

    l

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CARD_2 from "../../assets/images/card2.png"
 import { LuTrendingUpDown } from "react-icons/lu"
+import StatusInfoCard from '../StatusInfoCard'
 
 const AuthLayout = ({ children }) => {
   return (
@@ -36,18 +37,3 @@ const AuthLayout = ({ children }) => {
 
 export default AuthLayout
 
-const StatusInfoCard = ({icon , label , value ,color}) => {
-  return (
-    <div className='flex gap-5 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-200/50 z-10'>
-      <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
-        {icon}
-      </div>
-
-
-      <div>
-        <h6 className='text-xs text-gray-500 mb-1'>{label}</h6>
-        <span className='text-[20px]'>{value}</span>
-      </div>
-    </div>
-  )
-}
